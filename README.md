52558b0098e10672eaaf8456da521a20fda7275c
planetbridging <47130739+planetbridging@users.noreply.github.com>
2024-11-05 19:39:49 +1100

    successfully ran function of the blueprint!!!

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+git clone LayerForge in parent folder
+Then run to build the wasm
+
 ```
 GOOS=js GOARCH=wasm go build -o blueprint.wasm main.go
+```
+
+Then to host and test run
+
+```
+http://localhost:8000/
 ```
--- main.go
+++ main.go
@@ -1,66 +1,77 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"reflect"
-	"syscall/js"
-
-	"blueprint" // Import the blueprint package from LayerForge
+    "encoding/json"
+    "fmt"
+    "reflect"
+    "syscall/js"
+    "blueprint" // Adjust the import path if necessary
 )
 
-// Wrapper function to expose Blueprint methods to JavaScript
+// methodWrapper dynamically wraps each method of Blueprint to expose it to JavaScript
 func methodWrapper(bp *blueprint.Blueprint, methodName string) js.Func {
-	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		method := reflect.ValueOf(bp).MethodByName(methodName)
-		if !method.IsValid() {
-			return fmt.Sprintf("Method %s not found", methodName)
-		}
+    return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+        method := reflect.ValueOf(bp).MethodByName(methodName)
+        if !method.IsValid() {
+            return fmt.Sprintf("Method %s not found", methodName)
+        }
 
-		// Assume JSON input; parse args[0] as JSON if provided
-		if len(args) == 0 {
-			return "No parameters provided"
-		}
-		paramJSON := args[0].String()
-		var paramValues []interface{}
-		if err := json.Unmarshal([]byte(paramJSON), &paramValues); err != nil {
-			return fmt.Sprintf("Invalid JSON input: %v", err)
-		}
+        methodType := method.Type()
+        numParams := methodType.NumIn()
 
-		// Prepare reflect.Value inputs based on expected parameter types
-		inputs := make([]reflect.Value, len(paramValues))
-		for i, param := range paramValues {
-			inputs[i] = reflect.ValueOf(param)
-		}
+        var inputs []reflect.Value
 
-		// Call the method and capture results
-		results := method.Call(inputs)
+        if numParams > 0 { // Method expects parameters
+            if len(args) == 0 {
+                return fmt.Sprintf("Method %s requires parameters but none were provided", methodName)
+            }
 
-		// Format the results as JSON for output
-		output := make([]interface{}, len(results))
-		for i, result := range results {
-			output[i] = result.Interface()
-		}
-		resultJSON, _ := json.Marshal(output)
-		return string(resultJSON)
-	})
+            paramJSON := args[0].String()
+            var paramValues []interface{}
+            if err := json.Unmarshal([]byte(paramJSON), &paramValues); err != nil {
+                return fmt.Sprintf("Invalid JSON input: %v", err)
+            }
+
+            // Prepare reflect.Value inputs based on expected parameter types
+            inputs = make([]reflect.Value, numParams)
+            for i := 0; i < numParams; i++ {
+                if i < len(paramValues) {
+                    inputs[i] = reflect.ValueOf(paramValues[i])
+                } else {
+                    inputs[i] = reflect.Zero(methodType.In(i)) // Provide zero value for missing params
+                }
+            }
+        }
+
+        // Call the method with or without inputs, depending on numParams
+        results := method.Call(inputs)
+
+        // Format the results as JSON for output
+        output := make([]interface{}, len(results))
+        for i, result := range results {
+            output[i] = result.Interface()
+        }
+        resultJSON, _ := json.Marshal(output)
+        return string(resultJSON)
+    })
 }
 
 func main() {
-	// Create Blueprint instance
-	bp := &blueprint.Blueprint{}
+    // Create an instance of Blueprint
+    bp := &blueprint.Blueprint{}
 
-	// Retrieve all methods using reflection and expose them
-	methods, err := bp.GetBlueprintMethods()
-	if err != nil {
-		fmt.Println("Error getting methods:", err)
-		return
-	}
+    // Retrieve all methods using reflection from introspection
+    methods, err := bp.GetBlueprintMethods()
+    if err != nil {
+        fmt.Println("Error getting methods:", err)
+        return
+    }
 
-	for _, methodInfo := range methods {
-		js.Global().Set(methodInfo.MethodName, methodWrapper(bp, methodInfo.MethodName))
-	}
+    // Expose each method as a JavaScript function
+    for _, method := range methods {
+        js.Global().Set(method.MethodName, methodWrapper(bp, method.MethodName))
+    }
 
-	// Keep the WASM program running
-	select {}
+    // Keep the WebAssembly program running
+    select {}
 }
